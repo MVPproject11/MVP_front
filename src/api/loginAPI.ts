c2e17617c304d4a0b8efb6445d18c17bc6ee6dac
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-02-20 08:01:04 +0900

    수정

--- src/api/loginAPI.ts
+++ src/api/loginAPI.ts
@@ -1,6 +1,6 @@
 export async function loginAPI(email: string, password: string) {
     try {
-      const response = await fetch("https://localhost:8081/api/auth/login", {
+      const response = await fetch("http://localhost:8081/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
